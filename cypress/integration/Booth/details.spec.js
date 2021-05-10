a8c366ac600b53fe44badc88b38f192ac85d34ce
brightmanforever022 <brightmanforever@outlook.com>
2021-05-10 09:21:33 -0500

    change structure

--- cypress/integration/Booth/details.spec.js
+++ cypress/integration/Booth/details.spec.js
@@ -17,23 +17,26 @@ describe('check page contents', () => {
       .url({timeout: 20000}).should('include', '/booths')
   })
 
-  it('The title of the booth is present in the header', () => {
-    cy.contains('Demo Photo Booth')    
-  })
+  context('detail page', () => {
+    beforeEach(() => {
+      cy.get('a > p').contains('Demo Photo Booth', {timeout: 20000})
+        .click()
+        .url({timeout: 20000}).should('include', '/booth-details?id=')
+    })
+    it('The title of the booth is present in the header', () => {
+      cy.contains('Demo Photo Booth')
+    })
+  
+    it('Related galleries are visible', () => {
+      cy.contains('Demo Gallery')
+    })
 
-  it('Related galleries are visible', () => {
-    cy.contains('Demo Gallery')
-  })
-
-  it('Stop the booth', () => {
-    cy.get('a > p').contains('Demo Photo Booth', {timeout: 20000})
-      .click()
-      .url({timeout: 20000}).should('include', '/booth-details?id=');
-    cy.get('[data-test="Page header heading"]', {timeout: 20000})
-      .contains('Demo Photo Booth', {timeout: 20000});
-    cy.get('p').contains('Your Virtual Booth is up and running', {timeout: 20000});
-    cy.get('button > span').contains('Stop', {timeout: 20000}).click()
-      .should('have.value', 'Start')
-      .contains('Your Virtual Booth is stopped', {timeout: 100000});
+    it('Stop the booth', () => {
+      // cy.get('h5', {timeout: 30000}).contains('Demo Photo Booth')
+      // cy.get('div > p').contains('Your Virtual Booth is up and running')
+      cy.get('button > span', {timeout: 30000}).contains("Stop").click()
+        .should('have.value', 'Start', {timeout: 100000})
+        .contains('Your Virtual Booth is stopped');
+    })
   })
 })
