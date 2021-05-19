25b976587aee7ba5076ef29ff213d4f4b282adf2
brightmanforever022 <brightmanforever@outlook.com>
2021-05-19 04:05:00 -0500

    move login function to commands

--- cypress/integration/Booth/details.spec.js
+++ cypress/integration/Booth/details.spec.js
@@ -1,20 +1,6 @@
 describe('check page contents', () => {
   before(() => {
-    cy.intercept('GET', '/booths').as('boothsPage')
-    cy.visit('/login')
-    cy
-      .get('.MuiInputBase-input.MuiOutlinedInput-input')
-      .eq(0)
-      .type('whitehorse1990324@gmail.com')
-      
-    cy
-      .get('.MuiInputBase-input.MuiOutlinedInput-input')
-      .eq(1)
-      .type('irfan_farrel')
-    cy
-      .get('[data-test="Login button"]')
-      .click()
-      .url({timeout: 20000}).should('include', '/booths')
+    cy.loginByUsernamePassword();
   })
 
   context('detail page', () => {
