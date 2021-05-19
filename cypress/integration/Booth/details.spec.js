b880f4a06d1e380c140fbe687e14463730bfc781
brightmanforever022 <brightmanforever@outlook.com>
2021-05-19 03:54:49 -0500

    tests for publish button

--- cypress/integration/Booth/details.spec.js
+++ cypress/integration/Booth/details.spec.js
@@ -23,48 +23,54 @@ describe('check page contents', () => {
         .click()
         .url({timeout: 20000}).should('include', '/booth-details?id=')
     })
+
+    beforeEach(() => {
+      cy.wait(500)
+    })
     
-    // it('The title of the booth is present in the header', () => {
-    //   cy.get('[data-test="Page header heading"]')
-    //     .eq(0)
-    //     .contains('Booth 1')
-    // })
+    it('The title of the booth is present in the header', () => {
+      cy.get('[data-test="Page header heading"]')
+        .eq(0)
+        .contains('Booth 1')
+        .should('be.visible')
+    })
   
-    // it('Related galleries are visible', () => {
-    //   cy.contains('Gallery:')
-    //   cy.get('span > a')
-    //     .eq(0)
-    //     .contains('Gallery 1-1')
-    // })
+    it('Related galleries are visible', () => {
+      cy.contains('Gallery:')
+      cy.get('span > a')
+        .eq(0)
+        .contains('Gallery 1-1')
+        .should('be.visible')
+    })
 
-    // it('Stop the booth', () => {
-    //   cy.get('button > span').then(($buttons) => {
-    //     const buttonText = $buttons[0].innerText;
-    //     if(buttonText.includes('Stop')) {
-    //       console.log('stop')
-    //       cy.get('button')
-    //         .eq(0)
-    //         .contains('Stop')
-    //         .click({ force: true })
-    //     } else {
-    //       console.log('start')
-    //       cy.get('button')
-    //         .eq(0)
-    //         .contains('Start')
-    //         .click({ force: true })
-    //     }
-    //   })
-    // })
+    it('Stop the booth', () => {
+      cy.get('button > span').then(($buttons) => {
+        const buttonText = $buttons[0].innerText;
+        if(buttonText.includes('Stop')) {
+          console.log('stop')
+          cy.get('button')
+            .eq(0)
+            .contains('Stop')
+            .trigger("click")
+        } else {
+          console.log('start')
+          cy.get('button')
+            .eq(0)
+            .contains('Start')
+            .trigger("click")
+        }
+      })
+    })
 
-    // it('Capture link', () => {
-    //   cy.get('*[class^="MuiFormControl-root"]')
-    //     .find('input[type="text"]')
-    //     .eq(0)
-    //     .invoke('val')
-    //     .should(urlStr => {
-    //       expect(urlStr).to.equal("https://virtual.develop.doitselfie.eu/koda5aoi/a467edec-b4c0-4027-a34b-164811bc8fd2")
-    //     })
-    // })
+    it('Capture link', () => {
+      cy.get('*[class^="MuiFormControl-root"]')
+        .find('input[type="text"]')
+        .eq(0)
+        .invoke('val')
+        .should(urlStr => {
+          expect(urlStr).to.equal("https://virtual.develop.doitselfie.eu/koda5aoi/a467edec-b4c0-4027-a34b-164811bc8fd2")
+        })
+    })
 
     it('Captured link should work on page', () => {
       cy.get('*[class^="MuiFormControl-root"]')
@@ -78,13 +84,33 @@ describe('check page contents', () => {
         })
         .then((urlStr) => {
           cy.request(urlStr).its('body').should('include', 'Booth 1</h1>')
-          // cy.request(urlStr).then((response) => {
-          //   expect(response.status).to.eq(200)
-          //   expect(response).to.have.property('headers')
-          //   expect(response).to.have.property('duration')
-          // })
+        })        
+    })
+
+    it('check publish button (I will test by changing subheading)', () => {
+      let boothTitle = 'Subheading of Booth 1';
+      cy.get('*[placeholder^="Snap & share your selfie!"]')
+        .eq(0)
+        .wait(3000)
+        .clear()
+        .type(boothTitle)
+
+      cy.get('*[class^="MuiButton-label-"]')
+        .contains('Publish')
+        .trigger("click")
+        .then(() => {
+          cy.get('*[role="progressbar"]').should('be.visible', true)
         })
         
+      cy.wait(35000)
+
+      cy.get('*[class^="MuiFormControl-root"]')
+        .find('input[type="text"]')
+        .eq(0)
+        .invoke('val')
+        .then((urlStr) => {
+          cy.request(urlStr).its('body').should('include', 'Subheading of Booth 1</h2>')
+        })
     })
 
   })
